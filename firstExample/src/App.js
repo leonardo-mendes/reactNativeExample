d8d3cb7a92b934ccaebc47747a61a6d2704e7f72
Leonardo Mendes <leonardo.mendes@cvortex.io>
2020-06-26 20:30:22 -0300

    First Nav

--- firstExample/src/App.js
+++ firstExample/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Common from './style/Common';
-import ParImpar from './component/ParImpar'
 
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View>
-				<Text style={Common.ex}>First Example!</Text>
-				<ParImpar number={3} />
+				<Text style={Common.ex}>First Example!!</Text>
 			</View>
 		)
 	}
---
+++ firstExample/src/Menu.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './component/HomeScreen';
+import NotificationsScreen from './component/NotificationScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function Menu() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
